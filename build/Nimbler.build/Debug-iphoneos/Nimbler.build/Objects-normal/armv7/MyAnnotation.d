1c403de6aebfdaf240fdaf7f0a22ae60f0ca586d
John Canfield <john@networkcommuting.com>
2012-06-06 17:34:56 -0700

    Removing build directory

--- build/Nimbler.build/Debug-iphoneos/Nimbler.build/Objects-normal/armv7/MyAnnotation.d
+++
@@ -1,2 +0,0 @@
-dependencies: /github-tp/NC_iPhone/Network\ Commuting/MyAnnotation.m \
-  /github-tp/NC_iPhone/Network\ Commuting/MyAnnotation.h
--- build/Nimbler.build/Debug-iphonesimulator/Nimbler.build/Objects-normal/i386/PolylineEncodedString.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /github-tp/NC_iPhone/Network\ Commuting/PolylineEncodedString.m \
-  /github-tp/NC_iPhone/Network\ Commuting/PolylineEncodedString.h
