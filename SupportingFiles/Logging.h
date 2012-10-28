beb05822ae8f21a5a9ac4c70c1c801c024d613f7
John Canfield <john@networkcommuting.com>
2012-10-27 23:46:33 -0700

    Final update for Nimbler Caltrain v1.10 release  - Activated Flurry  - Turned off most console logging  - Changed to distribution provisioning profile

--- SupportingFiles/Logging.h
+++ SupportingFiles/Logging.h
@@ -12,11 +12,11 @@
 #define CUSTOM_RK_LOG_LEVELS RKLogLevelWarning  // Level of RestKit custom logging
 
 #define NIMLOG_PERF1(...)  // NSLog(__VA_ARGS__)  // Performance logging
-#define NIMLOG_EVENT1(...) NSLog(__VA_ARGS__) // Key events
+#define NIMLOG_EVENT1(...) // NSLog(__VA_ARGS__) // Key events
 #define NIMLOG_OBJECT1(...) // NSLog(__VA_ARGS__) // Extensive log printout of various objects
 #define NIMLOG_URLS(...) // NSLog(__VA_ARGS__)   // URL resources
 #define NIMLOG_ERR1(...) NSLog(__VA_ARGS__)   // Error / exception logging
-#define NIMLOG_FLURRY(...) NSLog(__VA_ARGS__)   // Logging of Flurry logs if flurry is not activated
+#define NIMLOG_FLURRY(...) // NSLog(__VA_ARGS__)   // Logging of Flurry logs if flurry is not activated
 #define NIMLOG_TWITTER1(...) // NSLog(__VA_ARGS__) // Routine Twitter advisory logging
 #define NIMLOG_ADDRESSES(...) // NSLog(__VA_ARGS__)  // Log addresses
 #define NIMLOG_DEBUG1(...) // NSLog(__VA_ARGS__) // Debugging logging
