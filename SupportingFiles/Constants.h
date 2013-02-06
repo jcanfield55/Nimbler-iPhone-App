8bd9626ae3722b1468a9cc88c469ffcfff1eedd4
jkumbhani <jkumbhani@gmail.com>
2013-02-06 05:52:18 -0800

    Changed local Url to live Url.

--- SupportingFiles/Constants.h
+++ SupportingFiles/Constants.h
@@ -17,7 +17,7 @@
 
 #define GEO_RESPONSE_URL      @"http://maps.googleapis.com/maps/api/geocode/"
 
-//#define TRIP_PROCESS_URL    @"http://23.23.210.156:8080/TPServer/ws/"
+#define TRIP_PROCESS_URL    @"http://23.23.210.156:8080/TPServer/ws/"
 
 //#define TEST_TRIP_PROCESS_URL @"http://localhost:4567/TPServer/ws/"  // Sinatra server for automated tests
 
@@ -27,7 +27,7 @@
 #define COREDATA_DB_FILENAME    @"store101.data"
 #define TEST_COREDATA_DB_FILENAME  @"testDataStore.data" // For automated tests
 
-#define TRIP_PROCESS_URL      @"http://192.168.3.196:6060/TPServer/ws/"
+//#define TRIP_PROCESS_URL      @"http://192.168.3.196:6060/TPServer/ws/"
 
 #define TWITTER_SERARCH_URL    @"https://twitter.com/#!/search/realtime/TRAIN%20from%3Acaltrain%20OR%20from%3Acaltrain_news"
 #define CALTRAIN_TWITTER_URL   @"https://twitter.com/#!/search/from%3Acaltrain%20OR%20from%3Acaltrain_news"
