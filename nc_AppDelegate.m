2044ee40bcba445d240780646e2b719801d5e3e9
Ashvin Patel <apatel@apprika.com>
2013-05-31 05:47:20 -0700

    Removed code to update the excludeFromSearch value in database at startup.

--- nc_AppDelegate.m
+++ nc_AppDelegate.m
@@ -321,20 +321,8 @@ FeedBackForm *fbView;
         if(!arrlocations || [arrlocations count] == 0){
             [stations generateNewTempLocationForAllStationString];
         }
-        // Fixed DE-337
-        // Added value of excludeFromSearch to NO if the excludeFromSearch is nil in db.
-#endif
-        NIMLOG_PERF2(@"Started Updating ExcludeFromSearch");
-        if([[[NSBundle mainBundle] bundleIdentifier] isEqualToString:CALTRAIN_BUNDLE_IDENTIFIER]){
-            NSFetchRequest * fetchLocation = [[NSFetchRequest alloc] init];
-            [fetchLocation setEntity:[NSEntityDescription entityForName:@"Location" inManagedObjectContext:self.managedObjectContext]];
-            NSArray * arrayLocations = [self.managedObjectContext executeFetchRequest:fetchLocation error:nil];
-            for (Location *loc in arrayLocations){
-                loc.excludeFromSearch = [NSNumber numberWithBool:NO];
-            }
-        }
-        NIMLOG_PERF2(@"Finished Updating ExcludeFromSearch");
         saveContext(self.managedObjectContext);
+#endif
         [toFromViewController setStations:stations];
     }@catch (NSException *exception) {
         logException(@"ncAppDelegate->didFinishLaunchingWithOptions #1", @"", exception);
