a3ae540d59a472cfdc3fb6836f175d8b638554c1
Ashvin Patel <apatel@apprika.com>
2013-07-10 06:05:07 -0700

    Added method that disable iCloud sharing of application document directory.

--- nc_AppDelegate.m
+++ nc_AppDelegate.m
@@ -119,6 +119,37 @@ FeedBackForm *fbView;
     return (__bridge_transfer NSString *)stringID;
 }
 
+// This methods will prevent document directory backups to icloud
+-(BOOL)addSkipBackupAttributeToItemAtURL:(NSURL *)URL
+{
+    const char* filePath = [[URL path] fileSystemRepresentation];
+    const char* attrName = "com.apple.MobileBackup";
+    if (&NSURLIsExcludedFromBackupKey == nil) {
+        // iOS 5.0.1 and lower
+        u_int8_t attrValue = 1;
+        int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
+        return result == 0;
+        
+    }
+    else {
+        // First try and remove the extended attribute if it is present
+        int result = getxattr(filePath, attrName, NULL, sizeof(u_int8_t), 0, 0);
+        if (result != -1) {
+            // The attribute exists, we need to remove it
+            int removeResult = removexattr(filePath, attrName, 0);
+            if (removeResult == 0) {
+                NSLog(@"Removed extended attribute on file %@", URL);
+            }
+        }
+        
+        // Set the new key
+        NSError *error = nil;
+        [URL setResourceValue:[NSNumber numberWithBool:YES] forKey:NSURLIsExcludedFromBackupKey error:&error];
+        return error == nil;
+    }
+}
+
+
 - (void) unzipZipFileToApplicationDocumentDirectory{
 #if GENERATING_SEED_DATABASE
     return ;
@@ -358,6 +389,7 @@ FeedBackForm *fbView;
     [FBSettings publishInstall:FB_APP_ID];
     NIMLOG_PERF2(@"Finished calling FB SDK");
     
+    [self addSkipBackupAttributeToItemAtURL:[self applicationDocumentsDirectory]];
     
     // Create an instance of a UINavigationController and put toFromViewController as the first view
     @try {
