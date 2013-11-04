d0d7ba3f789e8e75e7a290ed53e08feb9dca4741
John Canfield <john@networkcommuting.com>
2013-11-03 21:09:18 -0800

    Nimbler SF v1.35 release (submitted to app store)  - Turned on Flurry  - Turned off most logging  - Updated certificates  - Updated icon files

--- SupportingFiles/ConstantsCaltrain.h
+++ SupportingFiles/ConstantsCaltrain.h
@@ -141,7 +141,7 @@
 #define MAX_BIKE_DISTANCE_MAX_VALUE           20
 
 // Flurry analytics and logging
-#define FLURRY_ENABLED 0
+#define FLURRY_ENABLED 1
 #define IS_KICKFOLIO 0
 
 // Flurry events
